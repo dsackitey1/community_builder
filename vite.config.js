67e4abd077d8bec7144a95cdeebe3a9a014c4c4b
DESKTOP-OPG6A2A\hp <dsackitey01@gmail.com>
2024-01-26 19:22:44 +0000

    up

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/randomizer/",
+  base: "/community_builder/",
   plugins: [react()],
   build: {
     rollupOptions: {
